183fc8d35f222fc266705a8a34b2303e8307d43d
CamilGrace <156520346+CamilGrace@users.noreply.github.com>
2025-03-11 12:48:05 +0000

    Add old routing activity

--- LaravelActs/routes/web.php
+++ LaravelActs/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Simple Routing
+Route::get('/greet', function () {
+    return ('Hello, Laravel!');
+});
+
+Route::get('/greetings', [GreetController::class, 'greetPage']);
